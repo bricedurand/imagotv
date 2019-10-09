77afa9fb1d1a2de8580503df476d283f992c8d29
Brice <brice.durand2@gmail.com>
2019-10-09 16:30:00 +0200

    Go through each tvshow to get new videos

--- web/management/commands/look_up_new_content.py
+++ web/management/commands/look_up_new_content.py
@@ -8,7 +8,7 @@ import googleapiclient.discovery
 import googleapiclient.errors
 from django.conf import settings
 
-from web.models import ImagoInfoVideo
+from web.models import ImagoInfoVideo, ImagoInfoContent
 import isodate
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
@@ -21,17 +21,20 @@ class Command(BaseCommand):
         api_key = settings.YOUTUBE_API_KEY
 
         youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
-        request = youtube.playlistItems().list(
-            part="snippet,contentDetails",
-            fields = "items(id,snippet(title,publishedAt),contentDetails)",
-            playlistId="UU6-BWVphnrCj5xNL73qOd0w"
-        )
-        response = request.execute()
 
+        # Lookup all contents
+        for content in ImagoInfoContent.objects.filter(type='tvshow', source_id__isnull=False):
+            request = youtube.playlistItems().list(
+                part = "snippet,contentDetails",
+                fields = "items(id,snippet(title,publishedAt),contentDetails)",
+                playlistId = content.source_id
+            )
+            response = request.execute()
 
-        videos = []
-        published_after = datetime.now() - timedelta(days=7)
-        if response.get('items'):
+            # Only consider content from yesterday on
+            published_after = datetime.now() - timedelta(days=1)
+
+            # Create new video for each result
             for item in response.get('items'):
                 published_at = parse_yt_datetime(item['snippet'].get('publishedAt'))
                 if published_at <= published_after:
@@ -39,12 +42,15 @@ class Command(BaseCommand):
                 youtube_video_id = item['contentDetails']['videoId']
                 video = ImagoInfoVideo(publication_date=published_at,
                                        title=item['snippet']['title'],
+                                       content_id=content.content_id,
                                        thumbnail='youtube',
+                                       hosting='youtube',
                                        youtube_id=youtube_video_id,
                                        start_time=0,
                                        end_time=0,
                                        type='tvshow')
 
+                # Grab video duration through another API call
                 request = youtube.videos().list(
                     part="contentDetails",
                     id=youtube_video_id,
@@ -56,5 +62,4 @@ class Command(BaseCommand):
                 duration_in_seconds = isodate.parse_duration(duration).total_seconds()
                 video.duration = duration_in_seconds
 
-                # print(video)
                 video.save()
